11e2aeb06033953c38dbeaa01ca13f6a37c2b856
xelisme <173147634+xelisme@users.noreply.github.com>
2026-08-26 08:20:20 +0700

    menambah kenyamanan

---
+++ config/hypr-user.lua
@@ -0,0 +1,18 @@
+local vars = require("variables")
+local fn   = require("utils.functions")
+
+hl.bind(
+    vars.kbWallpaper,
+    hl.dsp.exec_cmd(
+        'if [ "$(qs -c caelestia ipc call drawers isOpen launcher)" != "1" ]; then '
+            .. "qs -c caelestia ipc call drawers toggle launcher; "
+            .. "sleep 0.3; "
+            .. 'ydotool type ">wallpaper "; '
+            .. "fi"
+    )
+)
+
+hl.bind("Print", hl.dsp.exec_cmd(os.getenv("HOME") .. "/.local/bin/screenshot-full"), { locked = true })
+
+hl.bind(vars.kbObsidianWs, fn.toggle("obsidian"))
+hl.bind(vars.kbBraveWs, fn.toggle("brave"))
---
+++ config/hypr-vars.lua
@@ -0,0 +1,13 @@
+return {
+	browser			= "brave",
+	terminal		= "kitty",
+	cursorTheme		= "GoogleDot-Black",
+	kbBrowser		= "",
+	kbTerminal		= "CTRL + ALT + T",
+	kbMoveWinToWs		= "SUPER + SHIFT",
+	kbWallpaper 		= "SUPER + W",
+	kbScreenshot		= "SUPER + SHIFT + ALT + P",
+	kbCloseWindow           = "ALT + F4",
+	kbObsidianWs		= "SUPER + O",
+	kbBraveWs		= "SUPER + B"
+}
